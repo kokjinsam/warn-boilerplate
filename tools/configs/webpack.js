46af8b882944884177c941075675957197f59ffa
skaxwng <samkj.ks@gmail.com>
2016-07-22 14:34:00 -0500

    Added more webpack configs

--- tools/configs/webpack.js
+++ tools/configs/webpack.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   hostname: process.env.HOSTNAME || 'localhost',
   port: 8080,
-  path: {
-    build: path.join(process.cwd(), 'build'),
-  },
+  srcPath: path.resolve(process.cwd(), 'src'),
+  nodeModulesPath: path.join(process.cwd(), 'node_modules'),
+  buildPath: path.resolve(process.cwd(), 'build'),
+  publicPath: '/',
 };
